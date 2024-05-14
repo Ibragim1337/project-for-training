99ca461946d5a226d84163f72c6d6840b98bdc76
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2024-05-14 21:22:31 +0400

    counter app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import './styles/counter.css'
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1)
+  }
+
+  const onClickMinus = () =>{
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
- 
+      <div>
+        <h2>Счетчик</h2>
+        <h1>{count}</h1>
+        <button className='minus' onClick={onClickMinus}>minus</button>
+        <button className='plus' onClick={onClickPlus}>plus</button>
+      </div>
     </div>
   );
 }
